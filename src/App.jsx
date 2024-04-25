69bd37250575412b602142f35a48442d51e86f9b
Shariar2006 <shariarnafis86@gmail.com>
2024-04-25 15:50:41 +0600

    start amrutam-appointment project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      ksldfk
+    </div>
+  )
+}
+
+export default App
---
+++ src/Router/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../MainLayout/MainLayout";
+import App from "../App";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+            }
+        ]
+    },
+]);
+
+export default router;
